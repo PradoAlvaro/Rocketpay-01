e939befc23c1d3dcd4f20481477c4e0467706ec5
Álvaro Prado <106787656+PradoAlvaro@users.noreply.github.com>
2023-01-13 22:12:30 -0300

    v

--- src/main.js
+++ src/main.js
@@ -47,10 +47,7 @@ const expirationDateMasked = IMask(expirationDate, expirationDatePattern)
 const cardNumber = document.querySelector("#card-number")
 const cardNumberPatter = {
   mask: [
-    {
-      mask: "0000 0000 0000 0000",
-      cardtype: "default",
-    },
+   
     {
       mask: "0000 0000 0000 0000",
       regex: /^4\d{0,15}/,
@@ -61,7 +58,10 @@ const cardNumberPatter = {
       regex: /(^5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\d{0,2})\d{0,12}/,
       cardtype: "mastercard",
     },
-
+    {
+      mask: "0000 0000 0000 0000",
+      cardtype: "default",
+    },
   ],
   dispatch: function (appended, dynamicMasked) {
     const number = (dynamicMasked.value + appended).replace(/\D/g, "")
